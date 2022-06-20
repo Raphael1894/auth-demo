37524e41b6bbb3f8510562e8ef99b3e4a16615eb
Raphael1894 <rapha.pereira.perso@gmail.com>
2022-06-20 09:42:40 +0200

    sign in user

--- utilities/auth.js
+++ utilities/auth.js
@@ -1,14 +1,22 @@
 import axios from "axios";
 
-const BACKEND_URL =
-  "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
 const API_KEY = "AIzaSyADZ2a2b7J7At78GJkeT1ShzYvaiBxGc1Y";
 
-export async function createUser(email, password) {
-  console.log(BACKEND_URL + API_KEY);
-  const response = await axios.post(BACKEND_URL + API_KEY, {
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
     email: email,
     password: password,
     returnSecureToken: true,
   });
+  console.log(response.data);
+}
+
+export async function createUser(email, password) {
+  await authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
 }
