42fe5f16bd08e1dd029345ec4e88355944d3499d
Raphael1894 <rapha.pereira.perso@gmail.com>
2022-06-20 09:56:40 +0200

    error messages on login / signup & auth context

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,13 +2,21 @@ import { useState } from "react";
 import AuthContent from "../components/auth/AuthContent";
 import { login } from "../utilities/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
-    await login(email, password);
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not log you in. Please try again"
+      );
+    }
     setIsAuthenticating(false);
   }
 
